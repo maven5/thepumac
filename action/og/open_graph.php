7f4592a3081ae4ef05cf1c445a1463754a059d5d
Gyu-Tae-Ann <world8888@naver.com>
2017-04-24 10:17:07 +0900

    1. 오픈그래프(og) 게시판 view페이지에 임시 적용

---
+++ action/og/open_graph.php
@@ -0,0 +1,10 @@
+<?php
+	$og_type= $_GET['og_type'];
+
+	$og_title = $_GET['og_title'];
+	$og_image = $_GET['og_image'];
+	$og_url = $_GET['og_url'];
+	$og_description = $_GET['og_description'];
+	
+	echo $og_type;
+?>
